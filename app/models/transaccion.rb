7edfb55e8ed2391db101f11c4aa5e73228c8a000
Leonardo Arias Paz <leoariaspaz@gmail.com>
2017-08-16 20:44:40 -0300

    Form Select y validaciones en ABM de transacciones
    Se agrega:
    - un select en el formulario de ABM de transacciones
    - validaciones al modelo Transaccion

--- app/models/transaccion.rb
+++ app/models/transaccion.rb
@@ -1,3 +1,13 @@
 class Transaccion < ApplicationRecord
+  validates :descripcion, presence: true, uniqueness: true
   belongs_to :tipo_transaccion
+  validate :tipo_transaccion_válido
+
+
+private
+	def tipo_transaccion_válido
+		if (not :tipo_transaccion_id.nil?) and TipoTransaccion.where(habilitado: true, id: :tipo_transaccion_id).empty?
+	 		errors.add(:tipo_transaccion_id, "es incorrecto")
+		end
+	end 
 end
