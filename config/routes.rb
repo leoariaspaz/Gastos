6f3d7a2b8b44accb047bfb4092641a0a9fb3959d
Leonardo Arias Paz <leoariaspaz@gmail.com>
2017-09-08 00:56:58 -0300

    Consolidado de movimientos
    Listado de saldos por cuenta.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   resources :cuentas
+  get 'consolidado', action: :consolidado, controller: :movimientos
   resources :movimientos do
     get 'cuenta/:id(/pag/:page)', action: :list, on: :collection
   end
